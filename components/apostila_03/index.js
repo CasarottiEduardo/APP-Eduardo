72b194c9707098ce22f64a9ea3c87fa0d3cddfa1
CasarottiEduardo <124743631+CasarottiEduardo@users.noreply.github.com>
2023-05-11 21:23:23 -0300

    aula 11/05

--- components/apostila_03/index.js
+++ components/apostila_03/index.js
@@ -1,19 +1,51 @@
+import  React, { useState} from 'react'; 
 import { Text, View , TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
+
 export default function apostila03() {
+
+  const[contador, setContador] = useState(0);
+
+ function AddNumber(){
+  setContador(contador + 1);
+  }
+
+  function subNumber(){
+    setContador(contador - 1);
+  }
+
+  function zerNumber(){
+    setContador(contador - contador);
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>Exemplo apostila 3</Text>
 
+      <Text style={styles.textCounter}>{contador}</Text>
+
       <TouchableOpacity 
         style={styles.button}
-        onPress={ () => alert('BATERIA FRACA')}
+        onPress={ () => AddNumber()}
       >
-        <Text style={styles.textButton}>Hello World!!</Text>
+        <Text style={styles.textButton}>Somar +1</Text>
       </TouchableOpacity>
-    
+
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={ () => subNumber()}
+      >
+        <Text style={styles.textButton}>Subtrair -1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={ () => zerNumber()}
+      >
+        <Text style={styles.textButton}>Zerar contador</Text>
+      </TouchableOpacity>
+
     </View>
 
   );
